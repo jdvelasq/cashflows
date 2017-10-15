b9fff29d854da083b0bca0d3d1aebd3f9c26edc0
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2017-10-15 17:22:19 -0500

    analysis conversion

--- cashflows/analysis.py
+++ cashflows/analysis.py
@@ -25,13 +25,83 @@ cashflows:
 """
 
 import numpy as np
+import pandas as pd
 
-from cashflows.gtimeseries import TimeSeries, cashflow, interest_rate, verify_eq_time_range
-from cashflows.rate import to_discount_factor, equivalent_rate
-from cashflows.common import _vars2list
-from cashflows.tvmm import tvmm
-from cashflows.utilityfun import exp_utility_fun, log_utility_fun, sqrt_utility_fun
+from timeseries import *
+from rate import *
+from common import _vars2list
+from tvmm import tvmm
+# from cashflows.utilityfun import exp_utility_fun, log_utility_fun, sqrt_utility_fun
 
+
+
+
+def irr(cflo):
+    """Computes the internal rate of return of a generic cashflow as a periodic
+    interest rate.
+
+    Args:
+        cflo (Series): Generic cashflow.
+
+    Returns:
+        Float or list of floats.
+
+    **Examples.**
+
+    >>> cflo = cashflow([-200] + [100]*4, start='2000Q1', freq='Q')
+    >>> irr(cflo) # doctest: +ELLIPSIS
+    34.90...
+
+    >>> irr([cflo, cflo]) # doctest: +ELLIPSIS
+    [34.90..., 34.90...]
+
+    """
+    if isinstance(cflo, pd.Series):
+        cflo = [cflo]
+    retval = []
+    for xcflo in cflo:
+        retval.append(100 * np.irr(xcflo.tolist()))
+    if len(retval) == 1:
+        return retval[0]
+    return retval
+
+## modified internal rate of return
+def mirr(cflo, finance_rate=0, reinvest_rate=0):
+    """Computes the modified internal rate of return of a generic cashflow
+    as a periodic interest rate.
+
+    Args:
+        cflo (list, cashflow): Generic cashflow.
+        finance_rate (float): Periodic interest rate applied to negative values of the cashflow.
+        reinvest_rate (float): Periodic interest rate applied to positive values of the cashflow.
+
+    Returns:
+        Float or list of floats.
+
+    **Examples.**
+
+    >>> cflo = cashflow([-200] + [100]*4, start='2000Q1', freq='Q')
+    >>> mirr(cflo) # doctest: +ELLIPSIS
+    18.92...
+
+    >>> mirr([cflo, cflo]) # doctest: +ELLIPSIS
+    [18.92..., 18.92...]
+
+
+    """
+    # negativos: finance_rate
+    # positivos: reinvest_rate
+    if isinstance(cflo, pd.Series):
+        cflo = [cflo]
+    retval = []
+    for xcflo in cflo:
+        retval.append(100 *  np.mirr(xcflo.tolist(),
+                                     finance_rate,
+                                     reinvest_rate))
+
+    if len(retval) == 1:
+        return retval[0]
+    return retval
 
 def timevalue(cflo, prate, base_date=0, utility=None):
     """
@@ -54,32 +124,32 @@ def timevalue(cflo, prate, base_date=0, utility=None):
     **Examples.**
 
 
-    >>> cflo = cashflow([-732.54] + [100]*8, pyr=4)
-    >>> prate = interest_rate([2]*9, pyr=4)
+    >>> cflo = cashflow([-732.54] + [100]*8, start='2000Q1', freq='Q')
+    >>> prate = interest_rate([2]*9, start='2000Q1', freq='Q')
     >>> timevalue(cflo, prate) # doctest: +ELLIPSIS
     0.00...
 
 
-    >>> prate = interest_rate([12]*5)
-    >>> cflo = cashflow([100]*5, spec = (0, -200))
+    >>> prate = interest_rate([12]*5, start='2000Q1', freq='Q')
+    >>> cflo = cashflow([-200]+[100]*4, start='2000Q1', freq='Q')
     >>> timevalue(cflo, prate) # doctest: +ELLIPSIS
     103.73...
 
     >>> timevalue(cflo, prate, 4) # doctest: +ELLIPSIS
     163.22...
 
-    >>> timevalue(cflo, prate, base_date=0, utility=exp_utility_fun(200)) # doctest: +ELLIPSIS
-    -84.15...
+    # >>> timevalue(cflo, prate, base_date=0, utility=exp_utility_fun(200)) # doctest: +ELLIPSIS
+    # -84.15...
 
-    >>> timevalue(cflo, prate, base_date=0, utility=log_utility_fun(210)) # doctest: +ELLIPSIS
-    369092793...
+    # >>> timevalue(cflo, prate, base_date=0, utility=log_utility_fun(210)) # doctest: +ELLIPSIS
+    # 369092793...
 
-    >>> timevalue(cflo, prate, base_date=0, utility=sqrt_utility_fun(210)) # doctest: +ELLIPSIS
-    2998.12...
+    # >>> timevalue(cflo, prate, base_date=0, utility=sqrt_utility_fun(210)) # doctest: +ELLIPSIS
+    # 2998.12...
 
 
-    >>> prate = interest_rate([12]*5)
-    >>> cflo = cashflow([-200] + [100]*4)
+    >>> prate = interest_rate([12]*5, start='2000Q1', freq='Q')
+    >>> cflo = cashflow([-200] + [100]*4, start='2000Q1', freq='Q')
     >>> timevalue(cflo=cflo, prate=prate) # doctest: +ELLIPSIS
     103.73...
 
@@ -103,11 +173,11 @@ def timevalue(cflo, prate, base_date=0, utility=None):
     base_date = params[2]
     retval = []
     for xcflo, xprate, xbase_date in zip(cflo, prate, base_date):
-        if not isinstance(xcflo, TimeSeries):
-            raise TypeError("`cflo` must be a TimeSeries")
-        if not isinstance(xprate, TimeSeries):
-            raise TypeError("`prate` must be a TimeSeries")
-        verify_eq_time_range(xcflo, xprate)
+        if not isinstance(xcflo, pd.Series):
+            raise TypeError("`cflo` must be a pandas.Series")
+        if not isinstance(xprate, pd.Series):
+            raise TypeError("`prate` must be a pandas.Series")
+        verify_period_range([xcflo, xprate])
         netval = 0
         factor = to_discount_factor(prate=xprate, base_date=xbase_date)
         for time, _ in enumerate(xcflo):
@@ -122,6 +192,7 @@ def timevalue(cflo, prate, base_date=0, utility=None):
     if len(retval) == 1:
         return retval[0]
     return retval
+
 
 
 def net_uniform_series(cflo, prate, nper=1):
@@ -139,13 +210,13 @@ def net_uniform_series(cflo, prate, nper=1):
 
     **Examples.**
 
-    >>> prate = interest_rate([2]*9, pyr=4)
-    >>> cflo = cashflow([-732.54] + [100]*8, pyr=4)
+    >>> prate = interest_rate([2]*9, start='2000Q1', freq='Q')
+    >>> cflo = cashflow([-732.54] + [100]*8, start='2000Q1', freq='Q')
     >>> net_uniform_series(cflo, prate) # doctest: +ELLIPSIS
     0.00...
 
-    >>> prate = interest_rate([12]*5)
-    >>> cflo = cashflow([-200] + [100]*4)
+    >>> prate = interest_rate([12]*5, start='2000Q1', freq='Q')
+    >>> cflo = cashflow([-200] + [100]*4, start='2000Q1', freq='Q')
     >>> net_uniform_series(cflo, prate) # doctest: +ELLIPSIS
     116.18...
 
@@ -195,13 +266,13 @@ def benefit_cost_ratio(cflo, prate, base_date=0):
 
     **Examples.**
 
-    >>> prate = interest_rate([2]*9, pyr=4)
-    >>> cflo = cashflow([-717.01] + [100]*8, pyr=4)
+    >>> prate = interest_rate([2]*9, start='2000Q1', freq='Q')
+    >>> cflo = cashflow([-717.01] + [100]*8, start='2000Q1', freq='Q')
     >>> benefit_cost_ratio(cflo, prate) # doctest: +ELLIPSIS
     1.02...
 
-    >>> prate = interest_rate([12]*5)
-    >>> cflo = cashflow([-200] + [100]*4)
+    >>> prate = interest_rate([12]*5, start='2000Q1', freq='Q')
+    >>> cflo = cashflow([-200] + [100]*4, start='2000Q1', freq='Q')
     >>> benefit_cost_ratio(cflo, prate) # doctest: +ELLIPSIS
     1.518...
 
@@ -227,7 +298,7 @@ def benefit_cost_ratio(cflo, prate, base_date=0):
 
     retval = []
     for xprate, xcflo, xbase_date in zip(prate, cflo, base_date):
-        verify_eq_time_range(xcflo, xprate)
+        verify_period_range([xcflo, xprate])
         num = 0
         den = 0
         num = xcflo.copy()
@@ -245,117 +316,54 @@ def benefit_cost_ratio(cflo, prate, base_date=0):
 
 
 
-def irr(cflo):
-    """Computes the internal rate of return of a generic cashflow as a periodic
-    interest rate.
-
-    Args:
-        cflo (TimeSeries): Generic cashflow.
-
-    Returns:
-        Float or list of floats.
-
-    **Examples.**
 
 
-    >>> cflo = cashflow([-717.01] + [100]*8, pyr=4)
-    >>> irr(cflo) # doctest: +ELLIPSIS
-    2.50...
+##-------------------
 
 
-    >>> cflo = cashflow([-200] + [100]*4)
-    >>> irr(cflo) # doctest: +ELLIPSIS
-    34.90...
-
-    >>> irr([cflo, cflo]) # doctest: +ELLIPSIS
-    [34.90..., 34.90...]
-
-    """
-    if isinstance(cflo, TimeSeries):
-        cflo = [cflo]
-    retval = []
-    for xcflo in cflo:
-        retval.append(100 * np.irr(xcflo.tolist()))
-    if len(retval) == 1:
-        return retval[0]
-    return retval
-
-## modified internal rate of return
-def mirr(cflo, finance_rate=0, reinvest_rate=0):
-    """Computes the modified internal rate of return of a generic cashflow
-    as a periodic interest rate.
-
-    Args:
-        cflo (list, cashflow): Generic cashflow.
-        finance_rate (float): Periodic interest rate applied to negative values of the cashflow.
-        reinvest_rate (float): Periodic interest rate applied to positive values of the cashflow.
-
-    Returns:
-        Float or list of floats.
-
-    **Examples.**
-
-    >>> cflo = cashflow([-200] + [100]*4)
-    >>> mirr(cflo) # doctest: +ELLIPSIS
-    18.92...
-
-    >>> mirr([cflo, cflo]) # doctest: +ELLIPSIS
-    [18.92..., 18.92...]
 
 
-    """
-    # negativos: finance_rate
-    # positivos: reinvest_rate
-    if isinstance(cflo, TimeSeries):
-        cflo = [cflo]
-    retval = []
-    for xcflo in cflo:
-        retval.append(100 *  np.mirr(xcflo.tolist(),
-                                     finance_rate,
-                                     reinvest_rate))
-
-    if len(retval) == 1:
-        return retval[0]
-    return retval
-
-def list_as_table(data):
-    """Prints the list `data` as a table. This function is used to produce a
-    human-readable format of a table for comparing financial indicators.
-
-    Args:
-        data (list): List of numeric values.
-
-    Returns:
-        None
-
-    **Example.**
-
-    >>> list_as_table(data=[1, 2, 3, 4]) # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
-     #               Value
-    ------------------------
-     0              1.0000
-     1              2.0000
-     2              3.0000
-     3              4.0000
-
-
-    >>> prate = interest_rate([12]*5)
-    >>> cflo = cashflow([-200] + [100]*4)
-    >>> list_as_table(timevalue(cflo=[cflo, cflo, cflo], prate=prate)) # doctest: +NORMALIZE_WHITESPACE
-     #               Value
-    ------------------------
-     0            103.7349
-     1            103.7349
-     2            103.7349
-
-    """
-    print(' #               Value')
-    print('------------------------')
-    #
-    data = [round(element, 4) for element in data]
-
-    for index, _ in enumerate(data):
-        print(' {:<3d}    {:14.4f}'.format(index, data[index]))
+#
+#
+#
+# def list_as_table(data):
+#     """Prints the list `data` as a table. This function is used to produce a
+#     human-readable format of a table for comparing financial indicators.
+#
+#     Args:
+#         data (list): List of numeric values.
+#
+#     Returns:
+#         None
+#
+#     **Example.**
+#
+#     >>> list_as_table(data=[1, 2, 3, 4]) # doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+#      #               Value
+#     ------------------------
+#      0              1.0000
+#      1              2.0000
+#      2              3.0000
+#      3              4.0000
+#
+#
+#     >>> prate = interest_rate([12]*5)
+#     >>> cflo = cashflow([-200] + [100]*4)
+#     >>> list_as_table(timevalue(cflo=[cflo, cflo, cflo], prate=prate)) # doctest: +NORMALIZE_WHITESPACE
+#      #               Value
+#     ------------------------
+#      0            103.7349
+#      1            103.7349
+#      2            103.7349
+#
+#     """
+#     print(' #               Value')
+#     print('------------------------')
+#     #
+#     data = [round(element, 4) for element in data]
+#
+#     for index, _ in enumerate(data):
+#         print(' {:<3d}    {:14.4f}'.format(index, data[index]))
 
 
 if __name__ == "__main__":
